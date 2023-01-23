7e683a9a798d54287011db684a34b79ea9a31e96
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-01-23 16:48:40 -0600

    Perform a Union on Two Sets Solution

---
+++ DataStructures/Sets/UnionOnTwoSets/index.js
@@ -0,0 +1,50 @@
+class Set {
+    constructor() {
+        // This will hold the set
+        this.dictionary = {};
+        this.length = 0;
+    }
+    // This method will check for the presence of an element and return true or false
+    has(element) {
+        return this.dictionary[element] !== undefined;
+    }
+    // This method will return all the values in the set
+    values() {
+        return Object.values(this.dictionary);
+    }
+    // This method will add an element to the set
+    add(element) {
+        if (!this.has(element)) {
+            this.dictionary[element] = element;
+            this.length++;
+            return true;
+        }
+
+        return false;
+    }
+    // This method will remove an element from a set
+    remove(element) {
+        if (this.has(element)) {
+            delete this.dictionary[element];
+            this.length--;
+            return true;
+        }
+
+        return false;
+    }
+    // This method will return the size of the set
+    size() {
+        return this.length;
+    }
+    // This method will perform a union on two sets
+    union(set) {
+        const newSet = new Set()
+        this.values().forEach(val => {
+            newSet.add(val)
+        })
+        set.values().forEach(val => {
+            newSet.add(val)
+        })
+        return newSet
+    }
+}
