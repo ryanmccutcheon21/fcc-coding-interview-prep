e7882429f6c338086068eb37638571a37b8f5912
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-02-02 18:45:54 -0600

    Create a Map Data Structure Solution

---
+++ DataStructures/CreateMapDataStructure/index.js
@@ -0,0 +1,27 @@
+const Map = function () {
+    this.collection = {}
+
+    this.has = key => {
+        return this.collection.hasOwnProperty(key)
+    }
+    this.add = (key, val) => {
+        this.collection[key] = val
+    }
+    this.remove = key => {
+        if (this.has(key)) {
+            delete this.collection[key]
+        }
+    }
+    this.get = key => {
+        return this.has(key) ? this.collection[key] : undefined
+    }
+    this.values = () => {
+        return [...Object.values(this.collection)]
+    }
+    this.size = () => {
+        return Object.entries(this.collection).length
+    }
+    this.clear = () => {
+        this.collection = {}
+    }
+}
