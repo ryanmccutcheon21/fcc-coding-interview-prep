1fa36a92d909505fc9f779f6c0dca79437bb707c
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-02-15 11:16:20 -0600

    Remove Elements from a Linked List by Index Solution

---
+++ DataStructures/LinkedList/RemoveElementsFromLinkedList/index.js
@@ -0,0 +1,60 @@
+function LinkedList() {
+    let length = 0
+    let head = null
+
+    const Node = function (element) {
+        this.element = element
+        this.next = null
+    }
+
+    this.size = function () {
+        return length
+    }
+
+    this.head = function () {
+        return head
+    }
+
+    this.add = function (element) {
+        let node = new Node(element)
+        if (head === null) {
+            head = node
+        } else {
+            let currentNode = head
+
+            while (currentNode.next) {
+                currentNode = currentNode.next
+            }
+
+            currentNode.next = node
+        }
+
+        length++
+    }
+
+    this.removeAt = function (index) {
+        // Exit early on bad input
+        if (index < 0 || index >= length) {
+            return null
+        }
+
+        // Find deleted node and remove
+        let deletedNode = head
+        if (index == 0) {
+            head = null
+        } else {
+            let currentNode = head
+            let currentIndex = 0
+            while (currentIndex < index - 1) {
+                currentNode = currentNode.next
+                currentIndex++
+            }
+            deletedNode = currentNode.next
+            currentNode.next = deletedNode.next
+        }
+
+        // Update and return
+        length--
+        return deletedNode.element
+    }
+}
